79c88ad6b68d088c39bdc43accc8fbbadce3b436
Marty Zhang <marty8zhang@gmail.com>
2021-08-09 21:38:49 +1000

    Create the project.

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'jest',
+  ],
+  rules: {
+    'class-methods-use-this': ['warn'],
+    'no-empty-function': 0,
+    '@typescript-eslint/no-empty-function': 1,
+    'no-shadow': 0,
+    '@typescript-eslint/no-shadow': 1,
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': 1,
+    'no-use-before-define': 0,
+    '@typescript-eslint/no-use-before-define': 1,
+    'no-useless-constructor': 0,
+    '@typescript-eslint/no-useless-constructor': 1,
+    semi: 0,
+    '@typescript-eslint/semi': 1,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+  },
+};
